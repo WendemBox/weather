454f3a8fac5630deb9b287ca7dedc572d5316db0
WendemBox <stepanfrolov1602@gmail.com>
2023-08-10 21:44:31 +0400

    Добавлен адаптив под iPhone 11

--- src/components/info.js
+++ src/components/info.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-
-class Info extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>Погодное приложение</h2>
-                <p>Узнайте погоду в своем городе!</p>
-            </div>
-        )
-    }
+class Info extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Приложение погоды</h2>
+        <p>Узнайте погоду в своем городе!</p>
+      </div>
+    );
+  }
 }
 
 export default Info;
