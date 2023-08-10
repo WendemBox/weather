2b7e462a0d47388603303f73a3b48033b7b6b4a5
WendemBox <stepanfrolov1602@gmail.com>
2023-08-10 18:38:25 +0400

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const API_KEY = "71e9c3eeba41664fae968f224657c27b";
+
+
+
+
+class App extends React.Component{
+
+state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+}
+
+    gettingWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+       
+
+        if(city) {
+            const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric}`);
+            const data = await api_url.json();
+
+            var sunset = data.sys.sunset;
+            var date = new Date();
+            date.setTime(sunset);
+            var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+        this.setState({
+            temp: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            sunset: sunset_date,
+            error: undefined
+         });
+        }   else{
+            this.setState({
+                temp: undefined,
+                city: undefined,
+                country: undefined,
+                sunset: undefined,
+                error: "Введите название города"
+             });
+        }
+         
+    }
+
+
+    render() {
+        return(
+            <div className="wrapper">
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-5"> <Info/></div>
+                        <div className="col-xs-7"><Form weatherMethod={this.gettingWeather} />
+                <Weather
+                    temp={this.state.temp}
+                    city={this.state.city}
+                    country={this.state.country}
+                    sunset={this.state.sunset}
+                    error={this.state.error}
+                /></div>
+                    </div>
+                </div>
+               
+
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/components/info.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Info extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Погодное приложение</h2>
+                <p>Узнайте погоду в своем городе!</p>
+            </div>
+        )
+    }
+}
+
+export default Info;
